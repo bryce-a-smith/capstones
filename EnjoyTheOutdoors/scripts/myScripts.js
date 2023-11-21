1eef364e071574190982cbdee37df98bda4f9ec0
BRYCE SMITH <a.bryce.smith.gmail.com>
2023-11-21 14:12:10 -0500

    displayPark working

--- EnjoyTheOutdoors/scripts/myScripts.js
+++ EnjoyTheOutdoors/scripts/myScripts.js
@@ -9,28 +9,8 @@ function init() {
   //Get html elements
   const searchBySelect = document.querySelector("#search-by-select");
   const criteriaSelect = document.querySelector("#criteria-select");
+  const parkListSelect = document.querySelector("#park-list-select");
   const displayP = document.querySelector("#display-p");
-
-  //Functions
-  // function getLocationList() {
-  //     let locationList = [];
-  //     nationalParksArray.forEach(park => {
-  //         let isPresent = false;
-  //         for (const l of locationList) {
-  //             if(park.LocationID == l.locationId) {
-  //                 isPresent = true;
-  //             }
-  //         }
-  //         if(!isPresent) {
-  //             let newLocation = {
-  //                 locationId: park.LocationID,
-  //                 locationName: park.LocationName
-  //             }
-  //             locationList.push(newLocation);
-  //         }
-  //     });
-  //     return locationList;
-  // }
 
   function clearParkList() {
     displayP.innerText = "";
@@ -44,8 +24,19 @@ function init() {
     criteriaSelect.appendChild(selectOption);
   }
 
+  function resetParkListSelect() {
+    parkListSelect.options.length = 0;
+
+    let displayOption = new Option("Display...", "");
+    let allOption = new Option("Display All", "all");
+
+    parkListSelect.appendChild(displayOption);
+    parkListSelect.appendChild(allOption);
+  }
+
   function loadCriteriaSelect(searchOption) {
     resetCriteriaSelect();
+    resetParkListSelect();
 
     if (searchOption == "location") {
       locationsArray.forEach((location) => {
@@ -62,9 +53,38 @@ function init() {
     }
   }
 
+  function loadParkListSelect(searchOption) {
+    clearParkList();
+
+    if (searchOption == "location") {
+      loadParksByLocation(criteriaSelect.value);
+    } else if (searchOption == "type") {
+      loadParksByType(criteriaSelect.value);
+    } else if (searchOption == "") {
+      clearParkList();
+    }
+  }
+
+  function loadParksByLocation(loc) {
+    let parkList = nationalParksArray.filter((p) => p.State == loc);
+    parkList.forEach((p) => {
+      let option = new Option(p.LocationName, p.LocationID);
+      parkListSelect.appendChild(option);
+    });
+  }
+
+  function loadParksByType(type) {
+    let parkList = nationalParksArray.filter((p) => p.LocationName.includes(type));
+    parkList.forEach((p) => {
+      let option = new Option(p.LocationName, p.LocationID);
+      parkListSelect.appendChild(option);
+    });
+  }
+
   function onSelectionChanged() {
     if (searchBySelect.value == "") {
       resetCriteriaSelect();
+      resetParkListSelect();
     } else {
       loadCriteriaSelect(searchBySelect.value);
     }
@@ -72,30 +92,47 @@ function init() {
 
   function onCriteriaSelectionChanged() {
     clearParkList();
-    loadCriteriaBasedList(searchBySelect.value);
+    resetParkListSelect();
+    loadParkListSelect(searchBySelect.value);
+  }
+
+  function onParkSelectionChanged() {
+    clearParkList();
+
+    populateParks();
+  }
+
+  function displayPark(park) {
+    displayP.innerText += `${park.LocationName}\n`;
   }
 
   function populateParksByLocation(loc) {
     let parkList = nationalParksArray.filter((p) => p.State == loc);
     parkList.forEach((park) => {
-      displayP.innerText += `${park.LocationName}\n`;
+      displayPark(park);
     });
   }
 
   function populateParksByType(type) {
     let parkList = nationalParksArray.filter((p) => p.LocationName.includes(type));
     parkList.forEach((park) => {
-      displayP.innerText += `${park.LocationName}\n`;
+        displayPark(park);
     });
   }
 
-  function loadCriteriaBasedList(searchOption) {
-    if (searchOption == "location") {
-      populateParksByLocation(criteriaSelect.value);
-    } else if (searchOption == "type") {
-      populateParksByType(criteriaSelect.value);
-    } else if (searchOption == "") {
-      clearParkList();
+  function populateParks() {
+    clearParkList();
+
+    if (parkListSelect.value == "all") {
+      if (searchBySelect.value == "location") {
+        populateParksByLocation(criteriaSelect.value);
+      } else if (searchBySelect.value == "type") {
+        populateParksByType(criteriaSelect.value);
+      }
+    } else {
+      let park = nationalParksArray.find((p) => p.LocationID == parkListSelect.value);
+
+      displayPark(park);
     }
   }
 
@@ -105,6 +142,7 @@ function init() {
   //Wire-up
   searchBySelect.addEventListener("change", onSelectionChanged);
   criteriaSelect.addEventListener("change", onCriteriaSelectionChanged);
+  parkListSelect.addEventListener("change", onParkSelectionChanged);
 }
 
 window.onload = init;
